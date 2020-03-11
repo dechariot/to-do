3262812510fca0c31f3338ffbc7875a72fe06440
dechariot <nsprisang@gmail.com>
2020-03-11 21:46:33 +0700

    fix toggle button error

--- js/script.js
+++ js/script.js
@@ -2,10 +2,9 @@ let masterTodoList = [];
 let addItemsBtn = document.getElementById("addItemsBtn");
 let inputItems = document.getElementById("inputItems");
 let allListArea = document.getElementById("allListArea")
-
+let doneListArea = document.getElementById("doneListArea")
 
 //Function
-
 let callToDo = () => {
     let tasks = {
         text: inputItems.value,
@@ -13,34 +12,45 @@ let callToDo = () => {
     }
     masterTodoList.push(tasks);
     allListArea.innerHTML = tasks.text;
-    update();   
+    update(masterTodoList,'all');   
 }
 
-let update = () => {
+let update = (array,to) => {
     let html = "";
-    for (i=0; i < masterTodoList.length;i++) {
-        html += `<li>${masterTodoList[i].text} <a style=" text-decoration: none;" href='#' onclick='remove(${i})'>x</a><a style=" text-decoration: none;" href='#' id="toggleBtn" onclick='toggleDone(${i})'>Toggle</a></li>\n`;}
-    allListArea.innerHTML = html;
+    for (i=0; i < array.length;i++) {
+        if (array[i].isDone===false){
+            sentence= `<li>${array[i].text} <a style=" text-decoration: none;" href='#' onclick='remove(${i})'>x</a><a style=" text-decoration: none;" href='#' id="toggleBtn" onclick='toggleDone(${i})'>Done</a></li>\n`;}
+            else if (array[i].isDone===true){
+                sentence= `<li>${array[i].text} <a style=" text-decoration: none;" href='#' onclick='remove(${i})'>x</a><a style=" text-decoration: none;" href='#' id="toggleBtn" onclick='toggleDone(${i})'>UnDone</a></li>\n`
+            }
+    html+=sentence
+    } 
+    if (to === 'all') {
+        allListArea.innerHTML = html;
+    } else if (to === 'done') {
+        doneListArea.innerHTML = html;
+    }    
     inputItems.value = "";
-
 }
-
 let remove = (item,index) => {
     masterTodoList.splice(index,1);
-    update();
+    update(masterTodoList,'all');
 }
-
 let toggleDone = (i) => {
-    if (masterTodoList[i].isDone===true){
-        document.getElementById("toggleBtn").innerHTML = `Undo Mark Done`;
-    } else if (masterTodoList[i].isDone===false){
-        document.getElementById("toggleBtn").innerHTML = `Mark Done`;
-    }
+
     masterTodoList[i].isDone = !(masterTodoList[i].isDone);
-    
+
+    updateDoneList();
+    update(masterTodoList,'all');
 }
 
-
+let updateDoneList = () => {
+    let doneList = masterTodoList.filter((item)=>{
+       return item.isDone==true
+    })
+    console.log(doneList)
+    update(doneList,'done');
+}
 
 //Event
 addItemsBtn.addEventListener("click",callToDo)
